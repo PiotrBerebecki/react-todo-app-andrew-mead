c8344536552d7df7b26886ab3c1d375a2a8efac3
PiotrBerebecki <petebere@gmail.com>
2016-08-29 01:13:20 +0200

    Fetch todos from firebase on app start

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -48,6 +48,25 @@ export var addTodos = (todos) => {
   };
 };
 
+export var startAddTodos = () => {
+  return (dispatch, getState) => {
+    var todosRef = firebaseRef.child('todos');
+    
+    return todosRef.once('value').then((snapshot) => {
+      var todos = snapshot.val() || {};
+      
+      var parsedTodos = Object.keys(todos).map((key) => {
+        return {
+          id: key,
+          ...todos[key]
+        };
+      });
+      
+      dispatch(addTodos(parsedTodos));
+    });    
+  };
+};
+
 export var updateTodo = (id, updates) => {
   return {
     type: 'UPDATE_TODO',
--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,22 +1,4 @@
 module.exports = {
-  setTodos: function (todos) {
-    if (Array.isArray(todos)) {
-      localStorage.setItem('todos', JSON.stringify(todos));
-      return todos; // undefined will be returned if invalid todos
-    }
-  },
-  getTodos: function () {
-    var stringTodos = localStorage.getItem('todos');
-    var todos = [];
-    // JSON.parse may fail on invalid data so we use try & catch block
-    try {
-      todos = JSON.parse(stringTodos);
-    } catch (e) {
-      // do nothing
-    }
-
-    return Array.isArray(todos) ? todos : [];
-  },
   filterTodos: function (todos, showCompleted, searchText) {
     var filteredTodos = todos;
     
