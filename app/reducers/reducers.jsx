5cb87294c627676d79661021a6bb96751101b137
PiotrBerebecki <petebere@gmail.com>
2016-08-30 12:48:36 +0200

    Add auth reducer and actions

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -36,12 +36,25 @@ export var todosReducer = (state = [], action) => {
         } else {
           return todo;
         }
-      });
+      });sss
     case 'ADD_TODOS':
       return [
         ...state,
         ...action.todos
       ];
+    default:
+      return state;
+  }
+};
+
+export var authReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        uid: action.uid
+      };
+    case 'LOGOUT':
+      return {};
     default:
       return state;
   }
